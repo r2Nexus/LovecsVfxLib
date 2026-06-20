ec432da54040f01dfaf1507761c9ae0f9fdf6e96
Vít Pokorný <pokorvi4@fit.cvut.cz>
2026-06-20 16:51:04 +0200

    Minor tweaks

---
+++ LovecsVfxLibCode/Vfx/Auras/LovecAuras.cs
@@ -0,0 +1,13 @@
+﻿namespace LovecsVfxLibCode.Vfx.Auras;
+
+public static class LovecAuras
+{
+    private const string Root = "res://LovecsVfxLib/scenes/vfx/auras/";
+
+    public const string Default = Root + "DefaultLovecAura.tscn";
+    public const string Enchanted = Root + "enchanted_aura.tscn";
+    public const string Guard = Root + "guard_aura.tscn";
+
+    public static string Custom(string fileName)
+        => Root + fileName;
+}
